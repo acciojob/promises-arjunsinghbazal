712729d427365a954f6533477888e522c7f9b3bb
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-03-30 15:20:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const promise = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve(10);
+	},1000)
+});
+promise.then(value=>{
+	console.log(value);
+	return value+10;
+}).then(value=>{
+	console.log(value);
+	return value+10;
+}).catch(value=>{
+	console.log('error:'+value);
+	return value+10;
+}).then(value=>{
+	console.log(value);
+	return value+10;
+});
+console.log('end');
